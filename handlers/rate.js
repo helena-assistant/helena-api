ca3e4ac23d1f1bd89390fb659f9eecf479e30845
montsouz <guilhermesouzamont@gmail.com>
2021-07-18 15:04:04 -0300

    chore(rate): status code

--- handlers/rate.js
+++ handlers/rate.js
@@ -1,32 +1,31 @@
 const responseHandler = require("../utils/response-handler");
 const parseRequest = require("../utils/parse-request");
-const updateRating = require("../repositories/rate/update")
-
+const updateRating = require("../repositories/rate/update");
 
 const handler = async (event) => {
-    const deps = handler.dependencies();
-    const {
-        body: { rate },
-    } = deps.parseRequest(event);
+  const deps = handler.dependencies();
+  const {
+    body: { rate },
+  } = deps.parseRequest(event);
 
-    if (isNaN(rate)) {
-        return responseHandler(400, { response: 'rate is not a number' });
-    }
+  if (isNaN(rate)) {
+    return responseHandler(400, { message: "rate is not a number" });
+  }
 
-    if (rate < 1 || rate > 5) {
-        return responseHandler(400, {
-            response: 'rate is invalid, please enter a value between 1 and 5'
-        });
-    }
+  if (rate < 1 || rate > 5) {
+    return responseHandler(400, {
+      message: "rate is invalid, please enter a value between 1 and 5",
+    });
+  }
 
-    await deps.updateRating({ rate })
+  await deps.updateRating({ rate });
 
-    return responseHandler(201, { response: 'OK' });
+  return responseHandler(200, { message: "OK" });
 };
 
 handler.dependencies = () => ({
-    updateRating,
-    parseRequest
+  updateRating,
+  parseRequest,
 });
 
 module.exports = { handler };
--- repositories/rate/update.js
+++ repositories/rate/update.js
@@ -1,32 +1,31 @@
 const storage = require("../../services/dynamoDB");
 
 const update = async ({ rate }) => {
-    const { storage } = update.dependencies();
+  const { storage } = update.dependencies();
 
-    const params = {
-        TableName: process.env.RATINGS_TABLE,
-        Key: {
-            pk: Number(rate),
-        },
-        UpdateExpression:
-            "ADD #counter :counter",
-        ExpressionAttributeValues: {
-            ":counter": 1,
-        },
-        ExpressionAttributeNames: {
-            "#counter": "counter",
-        },
-    };
+  const params = {
+    TableName: process.env.RATINGS_TABLE,
+    Key: {
+      pk: Number(rate),
+    },
+    UpdateExpression: "ADD #counter :counter",
+    ExpressionAttributeValues: {
+      ":counter": 1,
+    },
+    ExpressionAttributeNames: {
+      "#counter": "counter",
+    },
+  };
 
-    try {
-        return storage.update(params);
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    return storage.update(params);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 update.dependencies = () => ({
-    storage,
+  storage,
 });
 
 module.exports = update;
